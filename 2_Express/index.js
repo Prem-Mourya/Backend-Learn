2a4aaf50a82daaca2755a2702c619ad352936d9b
Prem Mourya <premmourya998@gmail.com>
2025-02-03 18:35:45 +0530

    Html render, middleware

--- 2_Express/index.js
+++ 2_Express/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-
+const morgan = require("morgan")
 const app = express();
 
+
+app.set("view engine", "ejs") // this is engine used to render html
+
+// Third party middleware morgan
+
+app.use(morgan("dev"));
+
+// middleware costume
+app.use((req, res, next) => {
+    console.log("this is middleware")
+    const a = 2
+    const b = 4
+    console.log(a+b)
+    return next()
+})
 app.get('/', (req, res) => {
-    res.send("This is Home page")
+    res.render("index")
 })
 app.get('/about', (req, res) => {
     res.send("This is about page")
